3757505eee3e77da75d1c2b81fd6ad2cb1b1b725
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-11-21 12:23:55 +0530

    wqwer

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "this_ubuntu" {
-  ami                    = "ami-0dee22c13ea7a9a67"  # x86_64 Ubuntu AMI ID
-  instance_type          = "t2.micro"  # x86_64 compatible instance type
-  key_name               = "mumbai_2"
-  count                  = 2
-  tags = {
-    name = "instance"
+      ami = "ami-0dee22c13ea7a9a67"  
+      instance_type = "t2.micro"  
+      key_name = "mumbai_2"
+      vpc_security_group_ids = ["sg-0b1ef1f40fb94ec14"]
+      count = 2
+      tags = {
+        name = "instance"
   }
 }
