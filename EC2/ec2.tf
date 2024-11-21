05b7adff9da76408770bc87410421667f290a551
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-11-21 11:35:27 +0530

    Update ec2.tf

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this_ubuntu" {
     ami = "ami-001f2488b35ca8aad"
     instance_type = "t4g.nano"
     key_name = "sydneysai"
-    vpc_security_group_ids = ["sg-03d74dc5f8101fbea"]
+    vpc_security_group_ids = ["sg-0b1ef1f40fb94ec14"]
     count = 2
     tags = {
       name = "instance"
