2d8f3488508da687211c6b85857a9a4711d5ab14
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-11-21 16:45:35 +0530

    module

--- Module_block/prod/module.tf
+++ Module_block/prod/module.tf
@@ -1,22 +1,21 @@
 module "ec2" {
-    source = "/home/cloudshell-user/Terraform/Module_block/Resources/EC2"
-    this_This_ami = "ami-0dee22c13ea7a9a67"
-    this_ec2type = "t2.micro"
-    This_key  = "mumbai_2"
-    this_disable_api_stop = false
-    this_disable_api_termination = false
-    #this_vpc_security_group_ids = "sg-0b1ef1f40fb94ec14"
-    this_aws_instance_subnet = module.aws_vpc.subnet_id
-    this_sg_vpc_id = module.aws_vpc.vpc_id
-    //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
+  source                  = "/home/cloudshell-user/Terraform/Module_block/Resources/EC2"
+  ami                    = "ami-0dee22c13ea7a9a67"
+  instance_type          = "t2.micro"
+  key_name               = "mumbai_2"
+  disable_api_stop       = false
+  disable_api_termination = false
+  vpc_security_group_ids = [module.aws_vpc.security_group_id]
+  subnet_id              = module.aws_vpc.subnet_id
+  vpc_id                 = module.aws_vpc.vpc_id
 }
 
 module "aws_vpc" {
-     source = "/home/cloudshell-user/Terraform/Module_block/Resources/VPC"
-     this_vpc_cidr_block = "192.168.0.0/16"
-     this_vpc_tags = "this_vpc"
-     this_subnet_pub_cidr_block = "192.168.0.0/17"
-     this_subnet_pub_map_ip  = true 
-     this_subnet_pub_tags = "pub_subnet"
-     this_vpc_az = "ap-south-1a"
+  source                     = "/home/cloudshell-user/Terraform/Module_block/Resources/VPC"
+  vpc_cidr_block             = "192.168.0.0/16"
+  vpc_tags                   = { Name = "this_vpc" }
+  subnet_pub_cidr_block      = "192.168.0.0/17"
+  subnet_pub_map_ip_on_launch = true
+  subnet_pub_tags            = { Name = "pub_subnet" }
+  availability_zone          = "ap-south-1a"
 }
