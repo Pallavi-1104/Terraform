23af2962dffb58a57e2fa968a53e0df0a4f8c5db
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-11-21 12:15:41 +0530

    qqqqqqqqqqqqq

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this_ubuntu" {
-  ami                    = "ami-0aebec83a182ea7ea"  # x86_64 Ubuntu AMI ID
+  ami                    = "ami-0dee22c13ea7a9a67"  # x86_64 Ubuntu AMI ID
   instance_type          = "t2.micro"  # x86_64 compatible instance type
   key_name               = "mumbai_2"
   count                  = 2
