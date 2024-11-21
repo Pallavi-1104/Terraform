eb370e267370ede622a52ee690529abbc0612df5
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-11-21 12:05:29 +0530

    keyname

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this_ubuntu" {
   ami                    = "ami-001f2488b35ca8aad"  # x86_64 Ubuntu AMI ID
   instance_type          = "t2.micro"  # x86_64 compatible instance type
-  key_name               = "sydneysai"
+  key_name               = "mumbai_2"
   count                  = 2
   tags = {
     name = "instance"
