2c244cff4e03b6bb216469235cd4ce971ccd9c4f
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-11-21 12:10:15 +0530

    qwerty

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this_ubuntu" {
-  ami                    = "ami-001f2488b35ca8aad"  # x86_64 Ubuntu AMI ID
+  ami                    = "ami-0aebec83a182ea7ea"  # x86_64 Ubuntu AMI ID
   instance_type          = "t2.micro"  # x86_64 compatible instance type
   key_name               = "mumbai_2"
   count                  = 2
